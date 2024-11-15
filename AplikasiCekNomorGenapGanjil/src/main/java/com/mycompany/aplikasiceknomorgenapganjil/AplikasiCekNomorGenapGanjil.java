6a779b283a9318e482b05a71c2413cf568057b1b
fadil-27 <fadilahmuhammad237@gmail.com>
2024-11-15 10:58:58 +0800

    menambahkan gui dan mendesain aplikasi

---
+++ AplikasiCekNomorGenapGanjil/src/main/java/com/mycompany/aplikasiceknomorgenapganjil/AplikasiCekNomorGenapGanjil.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplikasiceknomorgenapganjil;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class AplikasiCekNomorGenapGanjil {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
